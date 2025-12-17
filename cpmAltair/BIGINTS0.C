560725717154a5cc62e3009f7b83faccd1a845ab
Mendoza <998244@apsva.us>
2025-12-17 10:52:31 -0500

    fun

--- cpmAltair/BIGINTS0.C
+++ cpmAltair/BIGINTS0.C
@@ -13,11 +13,15 @@ int main()
 {
     struct bigint bint1, bint2, bint3;
     set_bigint("1234567", &bint1);
+    printf("bint1 is: %s\n", get_bigint(bint1));
     printf("bint1.negative is %d\n", bint1.negative);
     printf("bint1.numdigits is %d\n", bint1.numdigits);
     set_bigint("424", &bint2);
+    printf("bint2 is: %s\n", get_bigint(bint2));
     set_bigint("-17", &bint3);
+    printf("bint3 is: %s\n", get_bigint(bint3));
     set_bigint("65536", &bint3);
+    printf("bint3 is: %s\n", get_bigint(bint3));
 }
 
 void set_bigint(numstr, num)
@@ -35,4 +39,20 @@ struct bigint *num;
     printf("num->negative is %d\n", num->negative);
     printf("num->numdigits is %d\n", num->numdigits);
 
+}
+
+char* get_bigint(num)
+struct bigint *num;
+{
+   char *numstr;
+   char start_pos, i;
+   numstr = alloc(num->numdigits + (num->negative ? 2 : 1));
+   start_pos = num->negative;
+   if (start_pos == 1) numstr[0] = '-';
+   for (i = 0; i < num->numdigits; i++) {
+       numstr[i+start_pos] = num->digits[num->numdigits-(i+1)];
+       /* printf("numstr[%d] is %c\n", i, numstr[i+start_pos]); */
+   }
+   numstr[num->numdigits+start_pos] = '\0';
+   return numstr;
 }
