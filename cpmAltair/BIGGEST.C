072c8b818a8d601defbaf9dc8ea4a7fe78993dda
Mendoza <998244@apsva.us>
2025-12-17 10:30:10 -0500

    new tests + reasons

--- cpmAltair/BIGGEST.C
+++ cpmAltair/BIGGEST.C
@@ -13,13 +13,16 @@ main() {
     ubiggest++;
     printf("Add 1 to it and we get %u!\n", biggest);
     printf("Add 1 to it and we get %d!\n", ubiggest);
+    printf("\n^This has to do with byte overflow\n");
+    
     printf("--------------\nJeffTest\n");
     n =  65000;
     m = 3;
     prod = n * m;
     printf("%u * %u = %u -- Why?\n", m, n, prod);
+    printf("\n^This has to do with byte overflow\n");
 
-    printf("--------------\nTest 1\n");
+    printf("--------------\nTest 1 - Negativity\n");
     biggest = 0;
     ubiggest = 0;
     printf("The value of biggest is now %d.\n", biggest);
@@ -28,4 +31,17 @@ main() {
     ubiggest--;
     printf("Minus 1 to it and we get %u!\n", biggest);
     printf("Minus 1 to it and we get %d!\n", ubiggest);
+    printf("\n^This has to do with how the bytes that identify negativity are interpretted\n");
+
+    printf("--------------\nTest 2 - Adding Part two\n");
+    biggest = 65535;
+    printf("The value of biggest is now %d.\n", biggest);
+    biggest++;
+    printf("Add 1 to it and we get %u!\n", biggest);
+    biggest = 65534;
+    printf("The value of biggest is now %d.\n", biggest);
+    biggest++;
+    printf("Add 1 to it and we get %u!\n", biggest);
+    printf("\n^This has to do with the bytes that identify numbers are negative\n");
+
 }
