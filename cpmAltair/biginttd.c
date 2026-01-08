12f4a21a0d8015eebd2cd5ae1fd317aa586e6a7e
Mendoza <998244@apsva.us>
2026-01-08 11:10:23 -0500

    additional test

--- cpmAltair/biginttd.c
+++ cpmAltair/biginttd.c
@@ -11,6 +11,7 @@ main() {
     }
 
     TEST_CASE("Addition Test"){
+        /* 1234567 + 9999999 = 11234566 */
         struct bigint bi1;
         struct bigint bi2;
         struct bigint biout;
@@ -18,12 +19,21 @@ main() {
         set_bigint("1234567", &bi1);
         set_bigint("9999999", &bi2);
 
-        ASSERT_STR(get_bigint(bi1), "1234567");
-        ASSERT_STR(get_bigint(bi2), "9999999");
-
         add_bigint(bi1,bi2,biout);
 
         ASSERT_STR(get_bigint(biout), "11234566");
+
+        /* 987654321 + 123456789 = 1111111110 */
+        struct bigint bi3;
+        struct bigint bi4;
+        struct bigint biout2;
+
+        set_bigint("987654321", &bi3);
+        set_bigint("123456789", &bi4);
+
+        add_bigint(bi3,bi4,biout2);
+
+        ASSERT_STR(get_bigint(biout2), "1111111110");
     }
 
     END_TESTING();
